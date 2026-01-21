787ff2456a1643eb6a27c26d77b897114307f89e
Akash Panchal <akashpanchal22@gnu.ac.in>
2026-01-21 17:33:42 +0530

    Update default values in config.py

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'weather-app-secret-key')
-    OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY', 'dfd26ba5cfabdc6f7b53ea3f36f4c1ed')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'weather app password')
+
+    OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY', 'write your api key')
